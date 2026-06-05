3de128aaf6212cb263cfc92a91136767d28a71c4
Ali Bilal <MrAliBilal@outlook.com>
2026-06-05 20:13:08 +0500

    feat(dev-event): update index page and add LightRay component

--- NextJS/nextjsmastery/dev-event/app/page.tsx
+++ NextJS/nextjsmastery/dev-event/app/page.tsx
@@ -1,24 +1,27 @@
 import ExploreBtn from "@/components/ExploreBtn"
+import EventCard from "@/components/EventCard"
+import { events } from "@/lib/constants"
 
 const page = () => {
   return (
-      <section>
-        <h1 className="text-center">The Hub For Every Dev <br/> Event You Can't Miss</h1>
-        <p className="text-center">Hackathon, Meetups and Conferences All In One Place.</p>
-        
-        < ExploreBtn />
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
 
-        <div className="mt-20 space-x-7">
-          <h3>Featured Events</h3>
-          <ul className="events">
-            {[1, 2, 3, 4].map((event) => (
-              <li key={event} className="event-card">
-                <h4>Event {event}</h4>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </section>
+            <ExploreBtn />
+
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
+
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
   )
 }
 
